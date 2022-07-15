d9099057b79ae5f3bd7224938388ad50e371dfe6
pranavvdesai <pranavdesai.psd@gmail.com>
2022-07-15 19:50:28 +0530

    init

---
+++ contracts/NFT.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.1;
+
+// Import this file to use console.log
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+      Counters.Counter private _tokenIds;
+
+    constructor() ERC721 ("SquareNFT", "SQUARE") {
+    console.log("This is my NFT contract. Woah!");
+  }
+  // A function our user will hit to get their NFT.
+  function makeAnEpicNFT() public {
+     // Get the current tokenId, this starts at 0.
+    uint256 newItemId = _tokenIds.current();
+
+     // Actually mint the NFT to the sender using msg.sender.
+    _safeMint(msg.sender, newItemId);
+
+    // Set the NFTs data.
+    _setTokenURI(newItemId, "https://jsonkeeper.com/b/2BFV");
+    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+
+    // Increment the counter for when the next NFT is minted.
+    _tokenIds.increment();
+  }
+}
